b2bfa476688c485d48c39ebb2824c1d62770e3ff
Dorothy Ng <dng1@stuy.edu>
2016-11-02 21:07:18 -0400

    complete

--- dirInfo.c
+++ dirInfo.c
@@ -12,14 +12,24 @@
 
 int main(){
   printf("Statistics for directory: .\n");
+
   struct stat buf;
   stat(".",&buf);
+
   printf("Total Diectory Size: %lu Bytes\n",buf.st_size);
+
   DIR *dir = opendir(".");
+
   struct dirent *entry = readdir(dir);
   while (entry != NULL){
+    if (entry->d_type &DT_DIR){
+      printf("Directory | ");}
+    else {
+      printf("Regular File | ");}
     printf("%s\n", entry->d_name);
+    entry = readdir(dir);
   }
+
   closedir(dir);
   return 0;
 }
